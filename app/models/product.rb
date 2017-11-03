00af2678112d778ca88320bdc5438dfa30600868
Nora Moser <noramoser@Noras-MacBook-Pro.local>
2017-11-03 15:56:31 -0500

    starting on model tests for products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,19 @@
 require 'sqlite3'
 
 class Product
-end
+    attr_accessor :price, :quantity, :title, :description
+
+    def initialize(new_product_hash)
+        @price = new_product_hash[:info_price]
+        @quantity = new_product_hash[:info_quantity]
+        @title = new_product_hash[:info_title]
+        @description = new_product_hash[:info_description]
+    end
+
+    def create_new_product
+        begin
+            db = SQLite3::Database.open("../db/test_database_sprint2.sqlite")
+        rescue
+        end
+    end
+end 
