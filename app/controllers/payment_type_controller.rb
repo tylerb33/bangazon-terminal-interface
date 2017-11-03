385f5d9cfe06dc71dc0d7d9a4144587d9d552b16
Clayton Massie <claytonmassie@Claytons-MacBook-Pro.local>
2017-11-03 15:00:23 -0500

    added test and code for payment_type controller and payment_type modles

--- app/controllers/payment_type_controller.rb
+++ app/controllers/payment_type_controller.rb
@@ -1,4 +1,36 @@
 require_relative '../models/payment_type.rb'
 
 class PaymentTypeController
+
+attr_accessor :payment_type_info
+
+def initialize 
+    @payment_type_info = Hash.new
+end
+
+def gather_payment_info
+    get_account_number
+    get_account_type
+end
+
+    def get_account_number
+        puts "What account number would you like to use?"
+        print ">"
+        set_account_number(gets.chomp)
+    end
+
+    def get_account_type
+        puts "What account type would you like to use?"
+        print ">"
+        set_account_type(gets.chomp)
+    end
+
+    def set_account_number(account_number)
+        @payment_type_info[:account_number] = account_number
+    end
+
+    def set_account_type(account_type)
+        @payment_type_info[:account_type] = account_type
+    end
+
 end
--- app/test_model/payment_type_test.rb
+++ app/test_model/payment_type_test.rb
@@ -1,1 +1,57 @@
 require 'minitest/autorun'
+require_relative '../models/payment_type.rb'
+
+class PaymentTypeTest < Minitest::Test
+
+    def setup
+    # setup two things
+    @payment = PaymentType.new(
+
+        { 
+        :account_number => "1234567",  
+        :account_type => "Visa", 
+        } 
+
+        )
+    
+    end
+
+   # Test for instance of the object
+    # *****working*****
+    def test_initialize
+        assert_raises ArgumentError do 
+        PaymentType.new
+        end
+    end
+
+    # *****working*****
+    def test_initialize_for_arguments
+    assert_instance_of PaymentType, @payment 
+    end
+
+    # *****IN PROGRESS******
+    def test_create_payment_sql
+        new_customer_id = @customer.get_payment_info
+        assert_operator new_customer_id, :>, 0
+        # assert operator means that we're asserting this operator is true
+    end
+
+    # *****working*****
+    def test_get_all_payment
+        all_payment = @payment.get_all_payment
+        assert_instance_of Array, all_payment
+    end
+
+    # *****working*****
+    def test_get_single_customer
+        single_payment = @payment.get_single_payment(1)
+        assert_instance_of Array, single_payment
+    end
+
+    # *****working*****
+    def test_updating_payment
+        update_payment = @payment.update_single_payment("First_Name", 1)
+        assert_instance_of Array, update_payment
+    end
+
+end
