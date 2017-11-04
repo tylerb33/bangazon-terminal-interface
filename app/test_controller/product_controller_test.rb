9d272b179239dd791a16ffc4aa99002848d59327
Nora Moser <noramoser@Noras-MBP.hsd1.tn.comcast.net>
2017-11-04 10:01:26 -0500

    starting on controller tests

--- app/test_controller/product_controller_test.rb
+++ app/test_controller/product_controller_test.rb
@@ -1,1 +1,14 @@
-require 'minitest/autorun'
+require 'minitest/autorun' 
+
+class ProductTest < Minitest::Test
+
+    def setup
+        @product_controller = ProductController.new
+        # @product_test[:info_price] = 32.32
+        # @product_test[:info_quantity] = 3
+        # @product_test[:info_title] = "Kite"
+        # @product_test[:info_description] = "It is diamond shaped"
+    end
+
+    
+end
