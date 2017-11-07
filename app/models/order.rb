42e1f986c8148cb50805e412d941aa4bdb4c8cf2
LaKeshia Johnson <lakeshiajohnson@LaKeshias-MacBook-Pro.local>
2017-11-07 15:39:29 -0600

    ticket 6 WIP

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,6 +2,28 @@ require 'sqlite3'
 
 
 class Order
-	attr_accessor 
 
+
+	def get_payment_types_via_active_customer
+        begin
+        db = SQLite3::Database.open("../db/test_database_sprint2.sqlite")
+        customer_payment_type = db.execute("SELECT * FROM Payment_Types WHERE CustomerId = #{$active_customer[0]};")
+
+        db.close
+        return customer_payment_type
+        rescue SQLite3::Exception => e
+            p "Exception with database query: #{e}"
+        end
+    end
+
+
+=begin 
+   def insert_payment_type_into_order
+    	begin
+    	db = SQLite3::Database.open("../db/test_database_sprint2.sqlite")
+    	db.transaction
+    	db.execute("INSERT into Orders(Order_Id, Customer_Id_FK, Payment_Type_Id, Order_Date) VALUES () ")
+
+    end
+=end
 end
