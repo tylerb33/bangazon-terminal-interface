d356141e134381c106d45557126ce7e386f3763c
jordanberghofer <jordan.berghofer@gmail.com>
2017-11-07 11:39:31 -0600

    This is Noras work on ticket number 7.

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -2,10 +2,12 @@ require_relative '../models/product.rb'
 
 class ProductController
 
-    attr_accessor :product_info
+    attr_accessor :product_info, :product_model
 
     def initialize
         @product_info = Hash.new
+        @product_test = Product.new
+        @product_test2 = Product.new
     end
 
     # (below) this calls all of the methods so we can add just this to main menu
@@ -17,8 +19,12 @@ class ProductController
 
         @product_model = Product.new(@product_info) 
         @product_model.create_new_product 
+
+        
     end
 # Each of these are called in the main menu, and the gets takes the user input then the set methods puts the answer into a hash
+
+
     def get_product_price
         puts "How much does your product cost?"
         puts ">"
@@ -30,7 +36,7 @@ class ProductController
     end
 
     def get_product_quantity
-        puts "how many of this product do you wish to buy?"
+        puts "how many of this product do you have to sell?"
         puts ">"
         set_product_quantity(gets.chomp)
     end
@@ -58,4 +64,29 @@ class ProductController
     def set_product_description(description)
         @product_info[:info_description] = description
     end
+
+    # ***********************************************
+
+    def gather_all_products
+        @product_t = @product_test.get_products_via_active_customer
+        puts @product_t.class
+        @product_t.each_with_index do |value, index|
+            puts "#{index+1}. #{value[4]}"
+        end
+        puts "pick the product you wish to delete."
+        puts ">"
+        user_input = gets.chomp
+        user_input = user_input.to_i
+        $delete_user_input = user_input - 1
+        @product_t.each_with_index do |value, index|
+            if index == $delete_user_input
+                puts value[0]
+                @product_test2.delete_single_product(value[0])
+                puts "you have deleted that product"
+            end 
+        
+        end
+        
+    end
+
 end 
