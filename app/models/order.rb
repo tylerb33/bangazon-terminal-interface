d6cbb187539267a3133214c76ce5dbaae63a2fe4
Clayton Massie <claytonmassie@Claytons-MacBook-Pro.local>
2017-11-08 13:26:48 -0600

    added code for order_contriller

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,7 @@
 require 'sqlite3'
 
 
+
 class Order
 
 
@@ -16,6 +17,51 @@ class Order
         end
     end
 
+    def get_active_orders_for_customer
+        begin
+        db = SQLite3::Database.open("../db/test_database_sprint2.sqlite")
+        customer_active_orders = db.execute("SELECT * FROM Orders WHERE Customer_Id_FK = #{$active_customer[0]} AND Payment_Type_Id = null;")
+
+        db.close
+        return customer_active_orders
+        rescue SQLite3::Exception => e
+            p "Exception with database query: #{e}"
+        end
+    end
+
+
+    def make_order_id
+        begin
+        db = SQLite3::Database.open("../db/test_database_sprint2.sqlite")
+        db.transaction
+        db.execute("INSERT INTO Orders(Customer_Id_FK, Payment_Type_Id, Order_Date) VALUES (#{$active_customer[0]}, null, null);
+            SELECT last_insert_rowid();")
+        db.commit
+        id = db.last_insert_row_id 
+        db.close
+        return id
+        rescue SQLite3::Exception => e
+            p "Exception with database query: #{e}"
+            db.rollback
+        end
+    end
+
+
+    def get_current_order_id
+        begin
+        db = SQLite3::Database.open("../db/test_database_sprint2.sqlite")
+        db.transaction
+        db.execute("INSERT INTO Orders(Customer_Id_FK, Payment_Type_Id, Order_Date) VALUES (#{$active_customer[0]}, null, null);
+            SELECT last_insert_rowid();")
+        db.commit
+        id = db.last_insert_row_id 
+        db.close
+        return id
+        rescue SQLite3::Exception => e
+            p "Exception with database query: #{e}"
+            db.rollback
+        end
+    end
 
 =begin 
    def insert_payment_type_into_order
--- app/models/order_product.rb
+++ app/models/order_product.rb
@@ -3,4 +3,23 @@ require 'sqlite3'
 class OrderProduct
 
 	#attr_accessor
+
+	def initialize(**new_order_product_hash)
+		@order_id = new_order_product_hash[:info_order]
+		@product_id = new_order_product_hash[:info_product]
+	end
+
+	def create_orders
+	begin
+		db = SQLite3::Database.open("../db/test_database_sprint2.sqlite")
+		db.transaction
+		db.execute("INSERT INTO Order_Products(Order_Id, Product_Id) VALUES (#{@order_id}, #{@product_id})")
+		db.commit
+	rescue SQLite3::Exception => e
+		p "Exception with database query: #{e}"
+		db.rollback
+	end
+	db.close
+end
+
 end
